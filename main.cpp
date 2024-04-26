b559a1f839f3a150191491723ac33127e1f2b5db
r4qq <64530515+r4qq@users.noreply.github.com>
2024-04-26 08:46:58 +0200

    controls finally works

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <conio.h>
 #include "defs.hpp"
-
-
 
 struct positionPoint
 {
@@ -11,22 +10,40 @@ struct positionPoint
     int y;
 };
 
-class Level 
+class Level
 {
-    std::vector<std::vector<char>> LevelMap; 
+    std::vector<std::vector<char>> LevelMap;
     bool IsDone;
     unsigned int StepCount;
     std::vector<positionPoint> Targets;
     positionPoint StartPos;
-    public:
-       Level(std::vector<std::vector<char>> inMap)
-       {
-            LevelMap = inMap;
-       }
-       std::vector<std::vector<char>> GetMap()
-       {
-            return LevelMap;
-       }
+public:
+    Level(std::vector<std::vector<char>> inMap, int inStepCount, std::vector<positionPoint> inTargets, positionPoint inStartPos)
+    {
+        LevelMap = inMap;
+        IsDone = false;
+        StepCount = inStepCount;
+        Targets = inTargets;
+        StartPos = inStartPos;
+    }
+    std::vector<std::vector<char>> GetMap()
+    {
+        return LevelMap;
+    }
+    int GetStepCount()
+    {
+        return StepCount;
+    }
+    std::vector<positionPoint> GetTargets()
+    {
+        return Targets;
+    }
+    positionPoint GetStartPos()
+    {
+        return StartPos;
+    }
+
+
 };
 
 bool isLeveDone(std::vector<std::vector<char>> levelMap, std::vector<positionPoint> docks)
@@ -38,99 +55,124 @@ bool isLeveDone(std::vector<std::vector<char>> levelMap, std::vector<positionPoi
         if (levelMap[docks[i].x][docks[i].y] == DBOX)
             x++;
     }
-    bool isDone = (x == docksAmount) ? true : false;
-    return isDone; 
+    if (x == docksAmount) 
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
 }
 
 void renderLevel(std::vector<std::vector<char>> levelMap)
 {
     int levelWidth = levelMap.size();
     int levelHeight = levelMap[0].size();
-    
-    for(auto i = 0; i < levelWidth; i++)
+
+    for (auto i = 0; i < levelWidth; i++)
     {
-        for(auto j = 0; j < levelHeight; j++)
+        for (auto j = 0; j < levelHeight; j++)
         {
             std::cout << levelMap[i][j];
-        }    
+        }
         std::cout << "\n";
     }
 }
 
-void movePlayer(int dPX, int dPY, positionPoint pos, std::vector<std::vector<char>> currentMap)
+void movePlayer(int dPX, int dPY, positionPoint& pos, std::vector<std::vector<char>>& currentMap)
 {
-    int newPosX = pos.x + dPX;
-    int newPosY = pos.y + dPY;
-    
-    switch(currentMap[newPosX][newPosY])
+    int newPosX, newPosY;
+    newPosX = pos.x + dPX;
+    newPosY = pos.y + dPY;
+    bool movementMade = false;
+
+    switch (currentMap[newPosX][newPosY])
     {
+    default:
+        break;
+    
+    case AIR:
+        currentMap[pos.x][pos.y] = AIR;
+        currentMap[newPosX][newPosY] = P0;
+        movementMade = true;
+        break;
+    case BOX:
+        switch (currentMap[newPosX + dPX][newPosY + dPY])
+        {
         default:
+            movementMade = false;
+            break;
+        case BOX:
+            movementMade = false;
             break;
         case AIR:
             currentMap[pos.x][pos.y] = AIR;
             currentMap[newPosX][newPosY] = P0;
-            pos.x = newPosX;
-            pos.y = newPosY;
+            currentMap[newPosX + dPX][newPosY + dPY] = BOX;
+            movementMade = true;
             break;
-        case BOX:
-            switch (currentMap[newPosX + 1][newPosX + 1]) 
-            {
-                default:
-                    break;
-                case BOX:
-                    break;
-                case WALL:
-                    break;  
-                case AIR:
-                    currentMap[newPosX + 1][newPosY + 1] = BOX;
-                    currentMap[newPosX][newPosY] = P0;
-                    currentMap[pos.x][pos.y] = AIR;
-                    break;
-                case DOCK:
-                    break;
-            }
+        case DOCK:
+            currentMap[pos.x][pos.y] = AIR;
+            currentMap[newPosX][newPosY] = P0;
+            currentMap[newPosX + dPX][newPosY + dPY] = DBOX;
+            movementMade = true;
+            break;
+        }
+    }
+    if (movementMade)
+    {
+        pos.x = newPosX;
+        pos.y = newPosY;
     }
 }
 
 void game(positionPoint startPos, std::vector<positionPoint> targets, std::vector<std::vector<char>> levelMap, std::vector<positionPoint> doneBoxes)
 {
+    std::vector<std::vector<char>> localLevelMap = levelMap;
     positionPoint pozycja = startPos;
     bool levelDone = false;
     while (levelDone != true)
     {
-        system("cls");
-        renderLevel(levelMap);
-        char movement;
-        while(movement != 'm')
+
+        char movement = NULL;
+        while (movement != 'm')
         {
-            movement = getch();
-            switch(movement)
+            system("cls");
+            renderLevel(localLevelMap);
+            movement = _getch();
+            switch (movement)
             {
-                default:
-                    std::cout << "invalid move" << '\n';
-                    break;
-                case 'w':
-                    movePlayer(0, -1, pozycja, levelMap);
-                    break;
-                case 'a':
-                    movePlayer(-1, 0, pozycja, levelMap);
-                    break;
-                case 's':
-                    movePlayer(0, 1, pozycja, levelMap);
-                    break;
-                case 'd':
-                    movePlayer(1, 0, pozycja, levelMap);
-                    break;
+            default:
+                std::cout << "invalid move" << '\n';
+                break;
+            case 'w':
+                movePlayer(-1, 0, pozycja, localLevelMap);
+                break;
+            case 'a':
+                movePlayer(0, -1, pozycja, localLevelMap);
+                break;
+            case 's':
+                movePlayer(1, 0, pozycja, localLevelMap);
+                break;
+            case 'd':
+                movePlayer(0, 1, pozycja, localLevelMap);
+                break;
+            case 'm':
+                levelDone = true;
             }
+
+            levelDone = isLeveDone(levelMap, doneBoxes);
         }
-        levelDone = isLeveDone(levelMap, doneBoxes);
     }
+
 }
-
-
 
 int main()
 {
+    positionPoint pozycjaStartowa;
+    pozycjaStartowa.x = 5;
+    pozycjaStartowa.y = 4;
     std::vector<std::vector<char>> maplevel{
         {'*','*','*','*','*','*','*','*','*','*'},
         {'*','*','*','*','*','*','*','*','*','*'},
@@ -142,10 +184,15 @@ int main()
         {'*','*','*',' ',' ','*','*','*','*','*'},
         {'*','*','*','*','*','*','*','*','*','*'},
         {'*','*','*','*','*','*','*','*','*','*'}
-        }; 
-        
-    Level poz1(maplevel);
-    //renderLevel(poz1.GetMap());
-       
+    };
+
+    std::vector<positionPoint> targety{
+        {3, 3},
+        {2, 5}
+    };
+
+    Level poz1(maplevel, 50, targety, pozycjaStartowa);
+    game(poz1.GetStartPos(), poz1.GetTargets(), poz1.GetMap(), poz1.GetTargets());
+
     return 0;
 }
